ac094496e270f5184ecbc9fe53538b8b9051d581
Javier Pineda <javier@scientist.com>
2022-05-16 10:00:34 -0500

    rename substruct match test

--- tests/test_substruct.rs
+++ tests/test_substruct.rs
@@ -2,7 +2,7 @@ use rdkit::ROMol;
 use rdkit::substruct_match;
 
 #[test]
-fn test_enumerate_tautomer() {
+fn test_substruct_match() {
     let mol = ROMol::from_smile("c1ccccc1CCCCCCCC").unwrap();
     let query = ROMol::from_smile("C").unwrap();
     let does_it_match = substruct_match(&mol, &query);
