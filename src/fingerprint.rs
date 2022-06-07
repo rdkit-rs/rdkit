198f59f8acb7bee744a4db8db837860459c7db41
Xavier Lange <xrlange@gmail.com>
2022-06-07 14:19:18 -0400

    0.2.12 rdkit-sys function rename

--- src/fingerprint.rs
+++ src/fingerprint.rs
@@ -6,7 +6,7 @@ pub struct Fingerprint(pub BitVec<u64, bitvec::order::Lsb0>);
 
 impl Fingerprint {
     pub fn new(ptr: SharedPtr<rdkit_sys::fingerprint_ffi::ExplicitBitVect>) -> Self {
-        let unique_ptr_bytes = rdkit_sys::fingerprint_ffi::explicit_bit_vect_to_bytes_vec(ptr);
+        let unique_ptr_bytes = rdkit_sys::fingerprint_ffi::explicit_bit_vect_to_u64_vec(ptr);
         let rust_bytes: Vec<_> = unique_ptr_bytes.into_iter().map(|x| *x).collect();
         let bits = bitvec::vec::BitVec::<_, Lsb0>::from_vec(rust_bytes);
         Fingerprint(bits)
