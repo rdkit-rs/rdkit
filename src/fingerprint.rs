55d7d2e7175de9fb20e815613e565f838b3f7631
Xavier Lange <xrlange@gmail.com>
2022-12-16 14:38:07 -0500

    better null and exception handling

--- src/fingerprint.rs
+++ src/fingerprint.rs
@@ -12,6 +12,10 @@ impl Fingerprint {
         Fingerprint(bits)
     }
 
+    // pub fn as_bitvec_u8(&self) -> BitVec<u8, bitvec::order::Lsb0> {
+    //     self.0.iter().map(|big_word| )
+    // }
+
     pub fn tanimoto_distance(&self, other: &Fingerprint) -> f32 {
         let and = self.0.clone() & &other.0;
         let or = self.0.clone() | &other.0;
@@ -20,5 +24,39 @@ impl Fingerprint {
         let or_ones = or.count_ones();
 
         and_ones as f32 / or_ones as f32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use bitvec::prelude::*;
+    use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+
+    #[test]
+    fn convert_bitvect_u64_to_u8() {
+        // 283686952306183
+        let bytes: [u8; 8] = [
+            0b00000000, 0b00000001, 0b00000010, 0b00000011, 0b00000100, 0b00000101, 0b00000110,
+            0b00000111,
+        ];
+        let mut byte_reader = Cursor::new(&bytes[..]);
+        let bignum = byte_reader.read_u64::<BigEndian>().unwrap();
+
+        let bitvect_of_bignum: BitVec<u64, Lsb0> = BitVec::from_vec(vec![bignum]);
+
+        let vec_of_smallnum: Vec<u8> = bitvect_of_bignum
+            .iter()
+            .map(|bignum| {
+                let mut bytes = vec![];
+                bytes.write_u64(bignum);
+                bytes
+            })
+            .flatten()
+            .collect();
+        let bitvect_of_smallnum: BitVec<u8, Lsb0> = BitVec::from_vec(vec_of_smallnum);
+
+        panic!("{}", bignum);
     }
 }
