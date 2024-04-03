e95e3a06c95adef28060ddfa67eea3b073bf3569
Javier Pineda <javier@scientist.com>
2024-04-03 12:30:11 -0500

    add failing test for atom update_property_cache

--- tests/test_atom.rs
+++ tests/test_atom.rs
@@ -16,3 +16,14 @@ fn test_atom() {
     // assert_eq!(atoms[0].get_total_num_hs(), 100);
     // assert_eq!(atoms[0].get_total_valence(), 100);
 }
+
+#[test]
+fn test_atom_update_property_cache() {
+    let mut romol = rdkit::ROMol::from_smiles("C([H])([H])([H])([H])").unwrap();
+    let mut carbon = romol.atom_with_idx(0);
+    carbon.set_num_explicit_hs(5);
+
+    // This throws an error in c++ which is not captured in rust
+    // Note: the "strict" parameter checks the validity of the changes and errors out accordingly
+    carbon.update_property_cache(true);
+}
