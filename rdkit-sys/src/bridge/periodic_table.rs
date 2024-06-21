580c7aaa117582934f90447a6c71ed4fd64d2361
Marco Visani <92217074+mvisani@users.noreply.github.com>
2024-06-21 21:45:41 +0200

    added get_most_common_isotope_mass function to periodic table

--- rdkit-sys/src/bridge/periodic_table.rs
+++ rdkit-sys/src/bridge/periodic_table.rs
@@ -2,7 +2,7 @@
 pub mod ffi {
     unsafe extern "C++" {
         include!("wrapper/include/periodic_table.h");
-
         pub fn get_valence_list(atomic_number: u32) -> &'static CxxVector<i32>;
+        pub fn get_most_common_isotope_mass(atom: &CxxString) -> f64;
     }
 }
--- rdkit-sys/wrapper/src/periodic_table.cc
+++ rdkit-sys/wrapper/src/periodic_table.cc
@@ -6,4 +6,9 @@ const std::vector<int> &get_valence_list(unsigned int atomic_number) {
 	PeriodicTable *pt = RDKit::PeriodicTable::getTable();
 	return pt->getValenceList(atomic_number);
 }
+
+double get_most_common_isotope_mass(const std::string &symbol) {
+	PeriodicTable *pt = RDKit::PeriodicTable::getTable();
+	return pt->getMostCommonIsotopeMass(symbol);
+}
 } // namespace RDKit
