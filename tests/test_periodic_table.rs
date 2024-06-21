e40ae898e7c9422187d26b4507fd78930bc45446
Marco Visani <92217074+mvisani@users.noreply.github.com>
2024-06-21 21:45:41 +0200

    added get_most_common_isotope_mass function to periodic table

--- tests/test_periodic_table.rs
+++ tests/test_periodic_table.rs
@@ -5,3 +5,9 @@ fn test_valence_list() {
     let valence_list = PeriodicTable::get_valence_list(6);
     assert_eq!(&valence_list.iter().cloned().collect::<Vec<_>>(), &[4]);
 }
+
+#[test]
+fn test_most_common_isotope_mass() {
+    let mass = PeriodicTable::get_most_common_isotope_mass("C");
+    assert_eq!(mass, 12.00);
+}
