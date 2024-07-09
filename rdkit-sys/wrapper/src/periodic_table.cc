5d3c7a62f85910c33782e6999ce6979267e24bde
marco.visani@unifr.ch <marco.visani@unifr.ch>
2024-07-09 12:10:08 +0200

    reformatted cpp files according to clang-format

--- rdkit-sys/wrapper/src/periodic_table.cc
+++ rdkit-sys/wrapper/src/periodic_table.cc
@@ -3,12 +3,12 @@
 
 namespace RDKit {
 const std::vector<int> &get_valence_list(unsigned int atomic_number) {
-  PeriodicTable *pt = RDKit::PeriodicTable::getTable();
-  return pt->getValenceList(atomic_number);
+	PeriodicTable *pt = RDKit::PeriodicTable::getTable();
+	return pt->getValenceList(atomic_number);
 }
 
 double get_most_common_isotope_mass(const std::string &symbol) {
-  PeriodicTable *pt = RDKit::PeriodicTable::getTable();
-  return pt->getMostCommonIsotopeMass(symbol);
+	PeriodicTable *pt = RDKit::PeriodicTable::getTable();
+	return pt->getMostCommonIsotopeMass(symbol);
 }
 } // namespace RDKit
